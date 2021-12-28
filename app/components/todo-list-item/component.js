7582d170d42d6ecdcd43b03d2676a028e51edb31
Swaroop Gupta Voleti <swaroop.voleti@freshworks.com>
2021-12-28 17:26:49 +0530

    showing latest creatRecord on top of the list

--- app/components/todo-list-item/component.js
+++ app/components/todo-list-item/component.js
@@ -2,21 +2,15 @@ import Ember from "ember";
 const { set, computed, getProperties } = Ember;
 
 export default Ember.Component.extend({
-  listItem: computed("item", {
-    get() {
-      let { item } = getProperties(this, "item");
-      return item;
-    },
-  }),
-
+  
   actions: {
     toggleCompleted(e) {
-      let { listItem } = getProperties(this, "listItem");
-      set(listItem, "completed", e.target.checked);
+      let { item } = getProperties(this, "item");
+      set(item, "completed", e.target.checked);
     },
     removeTodo() {
-      let { listItem } = getProperties(this, "listItem");
-      listItem.deleteRecord();
+      let { item } = getProperties(this, "item");
+      item.deleteRecord();
     },
   },
 });
--- app/components/todo-list/component.js
+++ app/components/todo-list/component.js
@@ -1,4 +1,10 @@
 import Ember from "ember";
+const { set, computed, get } = Ember;
 
 export default Ember.Component.extend({
+  modelData: computed("model.@each.id", {
+    get() {
+      return get(this, "model").toArray().reverse();
+    },
+  }),
 });
