baeaf5571ea64b821f57b98752c865605dab5623
Swaroop Gupta Voleti <swaroop.voleti@freshworks.com>
2022-01-20 14:05:19 +0530

    Replaced controller with quary params for item status tabs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,19 @@
-import Ember from "ember"; 
+import Ember from "ember";
+const { computed } = Ember;
 
 export default Ember.Controller.extend({
+  queryParams: ["items"],
+  items: null,
+  todos: computed("items", "model", "model.@each.completed", function () {
+    let items = this.get("items");
+    let todoList = this.get("model");
+    if (items !== "all") {
+      return todoList.filterBy(
+        "completed",
+        items == "completed" ? true : false
+      );
+    } else {
+      return this.store.findAll("todo");
+    }
+  }),
 });
--- app/controllers/pending.js
+++
@@ -1,6 +0,0 @@
-import Ember from "ember";
-const { computed } = Ember;
-
-export default Ember.Controller.extend({
-  todos: computed.filterBy("model", "completed", false),
-});
